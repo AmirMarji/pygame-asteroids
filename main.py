a25b9751ae2e02946baaa75d51544d0bf817e27d
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2025-08-21 20:26:31 -0400

    black screen

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("start")
+import pygame
+import sys
+import random
+import math
+
+# init
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+
+# const colors
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+#gmae loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
+    screen.fill(BLACK)
+    pygame.display.flip()
+    clock.tick(60)
+
+
+pygame.quit()
+sys.exit()
